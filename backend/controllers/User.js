3266c22afb3d5539c333041057bdee4a669089e4
Alex <alexandreazzaro@gmail.com>
2021-03-05 14:17:43 +0000

    stop token

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import userSchema from "../models/User.js";
-import jwt from "jsonwebtoken"
+//import jwt from "jsonwebtoken"
 
 export default class userCtrl {
   signup = (req, res, next) => {
@@ -33,11 +33,11 @@ export default class userCtrl {
             }
             res.status(200).json({
               userId: user._id,
-              token: jwt.sign(
-                {userId: user._id},
+              /*token: jwt.sign(
+               {userId: user._id},
                 'Random_Token_Secret',
                 {expiresIn: '24h'}
-                ),
+                ),*/
             });
           })
           .catch((error) => res.status(500).json({ error }));
