4284fe3c9bc628cd17b2e7b8b9db419209ffece3
Alexandre <alexandre.ansel.98@gmail.com>
2021-03-10 12:46:23 +0100

    Changement mineur en html + ajout css

--- foto-front/src/Admin.js
+++ foto-front/src/Admin.js
@@ -1,70 +1,80 @@
 import React from 'react';
 import './css/Admin.css';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import SyncIcon from '@material-ui/icons/Sync';
 import Menu from './Menu';
 import { Redirect } from 'react-router-dom';
-
-class Row extends React.Component {
-    render() {
-        console.log(this.props)
-            return(
-                <tr>
-                    <td>{this.props.username}</td>
-                    {/* <td>{this.props.password}</td>
-                    <td>{this.props.mail}</td> */}
-                    <td>password</td>
-                    <td>mail</td>
-                    <td>delete</td>
-                </tr>
-            )
-    }
-}
 
 class Table extends React.Component {
 
     render() {
-        const users = ['Alexandre', 'Alexandre', 'Julien', 'Vladimir'];
-        const data = {
-            user1: {
+
+        function remove(user) {
+            console.log("Remove", user)
+        }
+
+        function changepw(user) {
+            console.log("Change password", user)
+        }
+
+        const data = [
+            {
                 username: "Alexandre",
                 password: "toto",
                 mail: "alexandre.ansel@gmail.com"
             },
-            user2: {
+            {
                 username: "Alexandre",
                 password: "titi",
                 mail: "alexandre.azzaro@gmail.com"
 
             },
-            user3: {
+            {
                 username: "Julien",
                 password: "tata",
                 mail: "julien.dupuis@gmail.com"
             },
 
-        }
+        ]
+
         let rows = [];
 
-        Object.keys(data).map((e, i) => {
+        data.map((user) =>
             rows.push(
-            <Row user={e} />
+                <tr>
+                    <td>{user.username}</td>
+                    <td>{user.mail}</td>
+                    <td>
+                        <IconButton aria-label="delete" onClick={() => changepw(user.username)}>
+                            <SyncIcon fontSize="large" />
+                        </IconButton>
+                    </td>
+                    <td>
+                        <IconButton aria-label="delete" onClick={() => remove(user.username)}>
+                            <DeleteIcon fontSize="large" />
+                        </IconButton>
+                    </td>
+                </tr>
             )
-        });
+        );
 
-        // data.map(user => {
-        //     console.log(user)
-        // })
 
-        // users.forEach(user => {
-        //     // console.log(user)
+        // for (let key in data) {
+        //     console.log(key);
         //     rows.push(
         //         <tr>
-        //             <td>{user}</td>
-        //             <td>mail</td>
-        //             <td>mdp</td>
-        //             <td>delete</td>
+        //             <td>{data[key].username}</td>
+        //             <td>{data[key].mail}</td>
+        //             <td>{data[key].password}</td>
+        //             <td>
+        //                 <IconButton aria-label="delete" onClick={remove}>
+        //                     <DeleteIcon fontSize="large" />
+        //                 </IconButton>
+        //             </td>
         //         </tr>
-        //     )
-        // });
+        //     );
+        // };
 
 
         return (
@@ -91,8 +101,8 @@ export default function Admin() {
     console.log("username : " + localStorage.getItem('username'));
     console.log("loggedIn : " + localStorage.getItem('loggedIn'));
 
-    // if(localStorage.getItem('loggedIn') !== 'true') {
-    if (localStorage.getItem('loggedIn') === 'true') {
+    if(localStorage.getItem('loggedIn') !== 'true') {
+    // if (localStorage.getItem('loggedIn') === 'true') {
         return <Redirect to="/login" />
     }
 
